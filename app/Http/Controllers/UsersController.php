92b3f49c7382a1ea83653c0559f5a6e2316fad07
joyleewei <1515885616@qq.com>
2018-02-09 16:20:36 +0800

    Basic user profile editing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 class UsersController extends Controller{
+
+    // 显示用户资料
     public function show(User $user){
         return view('users.show',compact('user'));
     }
 
-    public function update(){
+    // 编辑用户资料
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
 
+    // 更新用户资料
+    public function update(UserRequest $request,User $user){
+        $user->update($request->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功!');
     }
 }
