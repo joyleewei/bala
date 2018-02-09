8c6159f118a80bab41aafb6ab74ec70019cceab4
joyleewei <1515885616@qq.com>
2018-02-09 13:22:37 +0800

    Add captcha

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -2,3 +2,13 @@
 function route_class(){
     return str_replace('.','-',Route::currentRouteName());
 }
+
+function pr($var){
+    if(is_array($var)){
+        echo '<pre>';
+        print_r($var);
+        echo '</pre>';
+    }else{
+        var_dump($var);
+    }
+}
