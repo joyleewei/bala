79a275fa05876f261317beecbb3a300835da9bd6
joyleewei <1515885616@qq.com>
2018-02-11 15:27:00 +0800

    create categories

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UserRequest;
 use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller{
+
     public function __construct(){
         $this->middleware('auth',['except'=>['show']]);
     }
