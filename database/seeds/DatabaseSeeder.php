6de6e567beebacbd2aee18f53d50d849d0529221
joyleewei <1515885616@qq.com>
2018-02-13 13:07:27 +0800

    Seeding topics and users data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,18 +2,17 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run(){
         // php artisan db:seed --class=TopicsTableSeeder
         $this->call(UsersTableSeeder::class);
         // php artisan db:seed --class=TopicsTableSeeder
 		$this->call(TopicsTableSeeder::class);
     }
+
 }
